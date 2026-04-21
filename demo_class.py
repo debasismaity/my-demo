1c6543cba51f8de48dabd1ff4586f3b7e14f1357
emaideb <debasis.tcs@gmail.com>
2026-04-20 23:42:39 -0400

    Debasis

---
+++ demo_class.py
@@ -0,0 +1,20 @@
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def speak(self):
+        return f"{self.name} says {self.sound}"
+
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Woof")
+
+    def fetch(self, item):
+        return f"{self.name} fetches the {item}!"
+
+
+dog = Dog("Rex")
+print(dog.speak())
+print(dog.fetch("ball"))
